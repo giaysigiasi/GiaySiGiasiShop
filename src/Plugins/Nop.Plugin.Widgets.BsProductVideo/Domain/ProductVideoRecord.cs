b021b1725264439bdb60898e309a3c6a03426f62
David.Tran <david.tran@nexcel.vn>
2018-01-10 13:51:26 +0700

    Nop.Plugin.Widgets.BsProductVideo

---
+++ src/Plugins/Nop.Plugin.Widgets.BsProductVideo/Domain/ProductVideoRecord.cs
@@ -0,0 +1,14 @@
+using Nop.Core;
+
+namespace Nop.Plugin.Widgets.BsProductVideo.Domain
+{
+    /// <summary>
+    /// Represents a Google product record
+    /// </summary>
+    public partial class ProductVideoRecord : BaseEntity
+    {
+        public int ProductId { get; set; }
+        public string EmbedVideoHtmlCode { get; set; }
+        public int VideoThumbId { get; set; }
+    }
+}
